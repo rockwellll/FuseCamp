27bc7e542d5427d471197a4c6eed7018c4eb68e2
A7madXatab <Ahmadzendane725@gmail.com>
2021-01-13 21:51:44 +0300

    updating names from todo_list to todo_set

--- test/controllers/todo_lists_controller_test.rb
+++ test/controllers/todo_lists_controller_test.rb
@@ -4,7 +4,7 @@ class TodoListsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
   setup do
-    @todo_list = todo_lists :one
+    @todo_set = todo_lists :one
     @user = users :one
     @project = projects :one
   end
@@ -12,34 +12,34 @@ class TodoListsControllerTest < ActionDispatch::IntegrationTest
   test 'should show index for a project' do
     sign_in @user
 
-    get user_project_todo_lists_path user_id: @user, project_id: @project
+    get user_project_todo_sets_path user_id: @user, project_id: @project
     assert_response :success
   end
 
   test "should be able to create new todo lists" do
     sign_in @user
-    assert_difference 'TodoList.count' do
-      post user_project_todo_lists_path user_id: @user, project_id: @project, params: {todo_list: {name: "some random name"}}
+    assert_difference 'TodoSet.count' do
+      post user_project_todo_sets_path user_id: @user, project_id: @project, params: {todo_set: {name: "some random name"}}
     end
 
 
-    assert_redirected_to user_project_todo_list_path user_id: @user, project_id: @project, id: TodoList.last
+    assert_redirected_to user_project_todo_sets_path user_id: @user, project_id: @project, id: TodoSet.last
   end
 
   test "should be able to destroy a todo list" do
     sign_in @user
 
-    assert_difference 'TodoList.count', -1 do
-      delete user_project_todo_list_path user_id: @user, project_id: @project, id: @todo_list
+    assert_difference 'TodoSet.count', -1 do
+      delete user_project_todo_set_path user_id: @user, project_id: @project, id: @todo_set
     end
 
-    assert_redirected_to user_project_todo_lists_path user_id: @user, project_id: @project
+    assert_redirected_to user_project_todo_sets_path user_id: @user, project_id: @project
   end
 
   test 'should update a todo list' do
-    post user_project_todo_list_path user_id: @user, project_id: @project, id: @todo_list, params: {todo_list: {name: "updated name"}}
-    @todo_list.reload
+    post user_project_todo_set_path user_id: @user, project_id: @project, id: @todo_set, params: {todo_set: {name: "updated name"}}
+    @todo_set.reload
 
-    assert_equal 'updated name', @todo_list.name
+    assert_equal 'updated name', @todo_set.name
   end
 end
