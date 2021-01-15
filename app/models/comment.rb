778c19c97987a21c416bb8f8acd19d84b4010bbd
A7madXatab <Ahmadzendane725@gmail.com>
2021-01-15 23:29:16 +0300

    adding content validation along with rich text relationship

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   belongs_to :commentable, polymorphic: true
+  has_rich_text :content
+
+  validates :content, presence: true
 end
