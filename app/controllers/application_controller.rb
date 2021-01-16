14d3f9a90dec4d9d462c36239d3d9859258addbb
A7madXatab <Ahmadzendane725@gmail.com>
2021-01-16 23:15:45 +0300

    adding helpers from trubo and action_text for the tests to be able to use the helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
+require 'action_text'
+require 'hotwire-rails'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # importing them because of testing engine
+  helper ActionText::Engine.helpers
+  helper Turbo::FramesHelper
+  helper Turbo::StreamsHelper
 
   set_current_tenant_through_filter
   def set_tenant
