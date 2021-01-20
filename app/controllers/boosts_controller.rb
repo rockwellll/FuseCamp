52a940a01fe3907f673e7eafee737e8bc662b4fe
A7madXatab <Ahmadzendane725@gmail.com>
2021-01-20 20:42:53 +0300

    boost_controller to handle boost stuff

---
+++ app/controllers/boosts_controller.rb
@@ -0,0 +1,32 @@
+class BoostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_boost, only: [:destroy]
+
+  def create
+    @boost = @boostable.boosts.new boost_params
+    @boost.creator = current_user
+
+    respond_to do |format|
+      @boost.save
+      format.turbo_stream
+    end
+  end
+
+  def destroy
+    @boost.destroy
+
+    respond_to do |format|
+      format.turbo_stream { render 'boosts/remove'}
+    end
+  end
+
+  private
+
+  def set_boost
+    @boost = Boost.find params[:id]
+  end
+
+  def boost_params
+    params.require(:boost).permit :message
+  end
+end
