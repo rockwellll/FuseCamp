c28ee8f88fd311028d4517ebd8ae86f8bc3c2d89
A7madXatab <Ahmadzendane725@gmail.com>
2021-01-20 20:40:20 +0300

    adding new put_in_project_trash method

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -5,14 +5,19 @@ class Todo < ApplicationRecord
   belongs_to :todo_set
   has_rich_text :notes
 
-  # after_create_commit { broadcast_append_to "todos-#{todo_set.id}"}
-
   validates :name, presence: true
 
   scope :not_completed, -> { where.not status: true}
   scope :completed, -> { where status: true}
 
+  delegate :project, to: :todo_set
+
   def status_completed?
     status
+  end
+
+  def put_in_project_trash
+    update deleted_at: Time.now
+    project.put_in_trash self
   end
 end
