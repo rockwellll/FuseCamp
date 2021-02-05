10a42a094198d996db24de20096e3f88230f0852
A7madXatab <Ahmadzendane725@gmail.com>
2021-02-05 16:13:14 +0300

    adding content value, checking onconnect to either show or hide notification

--- app/javascript/controllers/notification_controller.js
+++ app/javascript/controllers/notification_controller.js
@@ -1,9 +1,25 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller{
-    static targets = ["container"]
+    static targets = ["container"];
+    static values= {
+        content: String
+    };
+
+    connect() {
+        if(this.contentValue) {
+            this.show();
+        } else {
+            this.element.classList.remove('show__notification')
+        }
+        super.connect();
+    }
+
+    show() {
+        this.element.classList.add('show__notification')
+    }
 
     hide() {
-        this.containerTarget.classList.replace('notification__pop__in', 'notification__pop__out');
+        this.element.classList.remove('show__notification');
     }
 }
