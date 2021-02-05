82076900695725faf2adbc03798cfe2b0ad9a6bd
A7madXatab <Ahmadzendane725@gmail.com>
2021-02-05 16:16:04 +0300

    adding recursive relationships into the model, making the model Trashable

--- app/models/todo_set.rb
+++ app/models/todo_set.rb
@@ -1,11 +1,17 @@
 class TodoSet < ApplicationRecord
-  include Commentable
+  include Commentable, Trashable
+
+  default_scope do
+    where parent_id: nil
+  end
 
   belongs_to :project
   belongs_to :creator, foreign_key: 'creator_id', class_name: 'User'
   has_rich_text :description
-
   has_many :todos
+
+  belongs_to :parent, class_name: 'TodoSet', optional: true
+  has_many :todo_sets, class_name: 'TodoSet', foreign_key: 'parent_id', dependent: :destroy
 
   before_destroy :destroy_all_todos
 
@@ -15,6 +21,11 @@ class TodoSet < ApplicationRecord
 
   def has_todos?
     todos.count != 0
+  end
+
+  def put_in_project_trash
+    update deleted_at: Time.now
+    project.put_in_trash self
   end
 
   def display_todos_status
