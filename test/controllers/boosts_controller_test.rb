f3f21c500207b900d0ec6d9fec8ea99621a1ca1b
A7madXatab <Ahmadzendane725@gmail.com>
2021-02-11 21:15:15 +0300

    adding tests for boosts_controller

--- test/controllers/boosts_controller_test.rb
+++ test/controllers/boosts_controller_test.rb
@@ -4,4 +4,29 @@ class BoostsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  #
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users :one
+    @comment = comments :one
+
+    sign_in @user
+  end
+
+  test 'it boosts a boostable' do
+    old_count = @comment.boosts.count
+
+    assert_difference 'Boost.count' do
+      post comment_boosts_path comment_id: @comment.id, params: {boost: {message: "some new boost"}}
+    end
+
+    assert_not_equal @comment.boosts.count, old_count
+  end
+
+  test "a boost can be destroyed" do
+    assert_difference 'Boost.count', -1 do
+      delete boost_path boosts(:one)
+    end
+  end
 end
