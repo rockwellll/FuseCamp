92648348f2e56f7d8ba7c4b828549186cf35ba25
A7madXatab <Ahmadzendane725@gmail.com>
2021-02-11 21:15:30 +0300

    adding tests for todo_groups_controller

--- test/controllers/todo_groups_controller_test.rb
+++ test/controllers/todo_groups_controller_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class TodoGroupsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+
+  setup do
+    @user = users :one
+    sign_in @user
+    @todo_set = todo_sets :one
+    @project = projects :one
+  end
+
+  test 'it creates a todo group for a todo set' do
+    assert_difference 'TodoSet.count' do
+      post todo_set_todo_groups_path todo_set_id: @todo_set, params: { todo_set: {name: "New todo group"}, color: 'red'}
+    end
+
+    assert_not_nil @todo_set.todo_groups.last.parent_id
+    assert_redirected_to user_project_todo_set_path user_id: @user, project_id: @project, id: @todo_set
+  end
+
+  test 'it will create a todo set based on a todo set and redirect to the todos page' do
+    assert_difference 'TodoSet.groups.count', -1 do
+      delete todo_set_promote_path todo_set_id: todo_sets(:three), params: { todo_set: {name: "New todo group"}, color: 'red'}
+    end
+  end
 end
