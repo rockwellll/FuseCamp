8fb82fd7c074c38407acef0a7b383c749e448324
A7madXatab <Ahmadzendane725@gmail.com>
2021-02-13 11:07:54 +0300

    new schema

--- db/schema.rb
+++ db/schema.rb
@@ -2,17 +2,24 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_07_212627) do
+ActiveRecord::Schema.define(version: 2021_02_10_070103) do
 
-  create_table "action_text_rich_texts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "accounts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
+
+  create_table "action_text_rich_texts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.text "body", size: :long
     t.string "record_type", null: false
@@ -22,7 +29,7 @@ ActiveRecord::Schema.define(version: 2021_02_07_212627) do
     t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
   end
 
-  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -32,7 +39,7 @@ ActiveRecord::Schema.define(version: 2021_02_07_212627) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "active_storage_blobs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -43,7 +50,7 @@ ActiveRecord::Schema.define(version: 2021_02_07_212627) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "boosts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "boosts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "boostable_type", null: false
     t.bigint "boostable_id", null: false
@@ -54,7 +61,7 @@ ActiveRecord::Schema.define(version: 2021_02_07_212627) do
     t.index ["user_id"], name: "index_boosts_on_user_id"
   end
 
-  create_table "colors", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "colors", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "hex", null: false
     t.string "colorable_type", null: false
     t.bigint "colorable_id", null: false
@@ -63,7 +70,7 @@ ActiveRecord::Schema.define(version: 2021_02_07_212627) do
     t.index ["colorable_type", "colorable_id"], name: "index_colors_on_colorable_type_and_colorable_id"
   end
 
-  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "commentable_type"
     t.bigint "commentable_id"
@@ -74,17 +81,27 @@ ActiveRecord::Schema.define(version: 2021_02_07_212627) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "project_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "companies", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
     t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_companies_on_user_id"
+  end
+
+  create_table "peoples", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "company_id", null: false
     t.bigint "project_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["project_id", "user_id"], name: "index_project_users_on_project_id_and_user_id"
-    t.index ["project_id"], name: "index_project_users_on_project_id"
-    t.index ["user_id"], name: "index_project_users_on_user_id"
+    t.index ["company_id", "user_id", "project_id"], name: "index_peoples_on_company_id_and_user_id_and_project_id", unique: true
+    t.index ["company_id"], name: "index_peoples_on_company_id"
+    t.index ["project_id"], name: "index_peoples_on_project_id"
+    t.index ["user_id"], name: "index_peoples_on_user_id"
   end
 
-  create_table "projects", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "projects", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "name"
     t.text "description"
@@ -93,21 +110,12 @@ ActiveRecord::Schema.define(version: 2021_02_07_212627) do
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
-  create_table "t1s", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "t1s", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "todo_groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.bigint "todo_set_id", null: false
-    t.bigint "parent_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["parent_id"], name: "index_todo_groups_on_parent_id"
-    t.index ["todo_set_id"], name: "index_todo_groups_on_todo_set_id"
-  end
-
-  create_table "todo_sets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "todo_sets", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "creator_id", null: false
     t.bigint "project_id", null: false
     t.string "name"
@@ -121,7 +129,7 @@ ActiveRecord::Schema.define(version: 2021_02_07_212627) do
     t.index ["project_id"], name: "index_todo_sets_on_project_id"
   end
 
-  create_table "todos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "todos", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "todo_set_id", null: false
     t.string "name", null: false
@@ -137,7 +145,7 @@ ActiveRecord::Schema.define(version: 2021_02_07_212627) do
     t.index ["user_id"], name: "index_todos_on_user_id"
   end
 
-  create_table "trash_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "trash_items", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "trash_id", null: false
     t.string "trashable_type"
     t.bigint "trashable_id"
@@ -147,14 +155,14 @@ ActiveRecord::Schema.define(version: 2021_02_07_212627) do
     t.index ["trashable_type", "trashable_id"], name: "index_trash_items_on_trashable_type_and_trashable_id"
   end
 
-  create_table "trashes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "trashes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "project_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["project_id"], name: "index_trashes_on_project_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -166,14 +174,15 @@ ActiveRecord::Schema.define(version: 2021_02_07_212627) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "accounts", "users"
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "boosts", "users"
   add_foreign_key "comments", "users"
-  add_foreign_key "project_users", "projects"
-  add_foreign_key "project_users", "users"
+  add_foreign_key "companies", "users"
+  add_foreign_key "peoples", "companies"
+  add_foreign_key "peoples", "projects"
+  add_foreign_key "peoples", "users"
   add_foreign_key "projects", "users"
-  add_foreign_key "todo_groups", "todo_sets"
-  add_foreign_key "todo_groups", "todo_sets", column: "parent_id"
   add_foreign_key "todo_sets", "projects"
   add_foreign_key "todo_sets", "todo_sets", column: "parent_id"
   add_foreign_key "todo_sets", "users", column: "creator_id"
