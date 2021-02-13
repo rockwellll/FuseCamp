ac87f5437d40b309fda031b267a950fd7a1c990d
A7madXatab <Ahmadzendane725@gmail.com>
2021-02-13 18:30:50 +0300

    a person belongs_to a account, an optional company and project, and delegates email and name to user

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,9 @@
 class Person < ApplicationRecord
   belongs_to :user
-  belongs_to :company
+  belongs_to :account
+  belongs_to :company, optional: true
+  belongs_to :project, optional: true
+
+  delegate :email, to: :user
+  delegate :name, to: :user
 end
