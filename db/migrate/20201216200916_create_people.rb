2f08a604f7c87a03b9495776444acb1dfaf6781d
A7madXatab <Ahmadzendane725@gmail.com>
2021-02-13 18:31:27 +0300

    changing the name from peoples to people

--- db/migrate/20201216200916_create_people.rb
+++ db/migrate/20201216200916_create_people.rb
@@ -1,6 +1,6 @@
-class CreatePeoples < ActiveRecord::Migration[6.1]
+class CreatePeople < ActiveRecord::Migration[6.1]
   def change
-    create_table :peoples do |t|
+    create_table :people do |t|
       t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :company, null: true, foreign_key: true
       t.belongs_to :project, null: true, foreign_key: true
@@ -8,7 +8,5 @@ class CreatePeoples < ActiveRecord::Migration[6.1]
 
       t.timestamps
     end
-
-    add_index :peoples, %i[company_id user_id project_id], unique: true
   end
 end
