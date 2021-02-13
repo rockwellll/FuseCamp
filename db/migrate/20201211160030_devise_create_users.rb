cba1fc3ab8ed9af6dbf2bced262d642efe3d582e
A7madXatab <Ahmadzendane725@gmail.com>
2021-02-13 18:30:08 +0300

    adding is_member and name column

--- db/migrate/20201211160030_devise_create_users.rb
+++ db/migrate/20201211160030_devise_create_users.rb
@@ -34,6 +34,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
 
 
       t.timestamps null: false
+      t.boolean :is_member, null: false
+      t.text :name, null: true
     end
 
     add_index :users, :email,                unique: true
