f813d6c0af080a7e7a1f478c5bc4a317dd88f90e
A7madXatab <Ahmadzendane725@gmail.com>
2021-02-14 21:03:11 +0300

    adding create action

--- app/controllers/peoples_controller.rb
+++ app/controllers/peoples_controller.rb
@@ -6,12 +6,31 @@ class PeoplesController < ApplicationController
     @members = @account.people
   end
 
+  def create
+    @person = Person.new person_params
+    @person.user.is_member = true
+    @person.account = @account
+
+    respond_to do |format|
+      if @person.save
+        format.html {  redirect_to account_peoples_path account_id: @account, notice: "New person added successfully"}
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
   def new
     @person = Person.new
+    @user = User.new is_member: true
   end
 
   private
   def set_account
     @account = Account.find params[:account_id]
+  end
+
+  def person_params
+    params.require(:person).permit(:job_title, user_attributes: [:name, :email, :password, :is_member])
   end
 end
