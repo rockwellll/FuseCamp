e1957929456c805c2d02ccab2805fdddcfa5148d
A7madXatab <Ahmadzendane725@gmail.com>
2021-02-15 11:48:39 +0300

    changed the tests to use the account insted of the user

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -4,19 +4,19 @@ class ProjectsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
   setup do
-    @user = users :one
-    @project = @user.projects.first
+    @account = accounts :one
+    @project = @account.projects.first
   end
 
   test 'when getting the root path youll be redirected to the projects path of a user' do
     sign_in users(:one)
     get root_path
-    assert_redirected_to user_projects_path user_id: users(:one)
+    assert_redirected_to account_projects_path account_id: @account
   end
 
   test "should get new form for creating a new project" do
     sign_in users :one
-    get new_user_project_path user_id: users(:one)
+    get new_account_project_path account_id: @account
   end
 
   test "creating a new project will redirect back to the projects page" do
@@ -25,32 +25,29 @@ class ProjectsControllerTest < ActionDispatch::IntegrationTest
         name: "new project",
         description: "some random description"
     }
-
-
     assert_difference('Project.count') do
-      post user_projects_path user_id: users(:one), params: { project: new_project }
+      post account_projects_path account_id: @account, params: { project: new_project }
     end
 
-    assert_redirected_to user_projects_path user_id: users(:one)
+    assert_redirected_to account_projects_path account_id: @account
   end
 
   test "updating projects will redirect to the project index path" do
-    sign_in @user
-    project = @project
-    patch user_project_path user_id: @user, id: project, params: { project: {name: "updated"} }
+    sign_in users(:one)
+    patch account_project_path account_id: @account, id: @project, params: { project: {name: "updated"} }
 
-    project.reload
+    @project.reload
 
-    assert_redirected_to user_projects_path user_id: @user
-    assert_equal project.name, "updated"
+    assert_redirected_to account_projects_path account_id: @account
+    assert_equal @project.name, "updated"
   end
 
   test "deleting a project will redirect to projects index path" do
-    sign_in @user
+    sign_in users(:one)
     assert_difference 'Project.count', -1 do
-      delete user_project_path user_id: @user, id: @project
+      delete account_project_path account_id: @account, id: @project
     end
 
-    assert_redirected_to user_projects_path user_id: @user
+    assert_redirected_to account_projects_path account_id: @account
   end
 end
